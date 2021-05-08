3b7bd6036086ffea9c6dd88db27d90e79b6183a6
Kermit23Jaba <aurfree@gmail.com>
2021-05-08 19:33:12 +0300

    one

--- 1.rb
+++ 1.rb
@@ -1,1 +1,14 @@
-puts "Hello"
+def reverse_string(string)
+  loop = string.size
+  word = ''
+  while loop > 0
+    loop -= 1                  
+    word += string[loop]
+  end
+
+word.split.map(&:chars) { |i| if i == "a" i = 'n'end}
+
+  return puts word
+end
+
+reverse_string("abs")
