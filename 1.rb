79e0feafc527cb49466e929dfaf7dbbb28c88b6c
Kermit23Jaba <aurfree@gmail.com>
2021-05-09 17:48:58 +0300

    master

--- 1.rb
+++ 1.rb
@@ -1,14 +1,51 @@
-def reverse_string(string)
-  loop = string.size
-  word = ''
-  while loop > 0
-    loop -= 1                  
-    word += string[loop]
-  end
+# def reverse_string(string)
+#   loop = string.size
+#   word = ''
+#   while loop > 0
+#     loop -= 1                  
+#     word += string[loop]
+#   end
 
-word.split.map(&:chars) { |i| if i == "a" i = 'n'end}
+#   return puts word
+# end
 
-  return puts word
-end
+# reverse_string("abs")
 
-reverse_string("abs")
+# a = "abcd"
+# b = []
+# a.split(//).each do |i|
+#   if i == "a"
+#     i = "N"
+#     b << i
+#   elsif 
+#     b << i
+#   end
+# end
+
+# puts b.join
+    
+
+# a = 'absc'
+# b = []
+# for i in a.split(//) do
+#   if i == "a" 
+#     i = "A"
+#     b << i
+#   elsif
+#     b << i
+#   end
+# end
+
+# puts b.join
+
+
+
+"hello".each_char {|c| print c, if c == "e"; с = "J"; b +=c}
+puts b
+
+# a = 'abcd'.split(//).map { |ch| ch == 'a' ? 'n' : ch }.join
+# puts a
+
+result = ''
+"hello".each_char { |c| c == 'e' ? result += 'n' : result += c }
+print result
