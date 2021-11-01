631577bc60973ebb62d4c44c81fbf16b1f35c1d1
Kermit23Jaba <aurfree@gmail.com>
2021-11-01 12:35:15 +0500

    one

--- SI/Lesson20/app.rb
+++ SI/Lesson20/app.rb
@@ -6,5 +6,17 @@ end
 
 
 post '/' do
+    @user_name = params[:user_name]
+    @phone = params[:phone]
+    @data = params[:data]
     erb :index
+
+    @title = "Thank you"
+    @message = "Dear #{@user_name}, your phone #{@phone} at #{@data}"
+
+    f = File.open('users.txt', 'a')
+    f.write("#{@user_name} #{@phone} #{@data}\n")
+    f.close
+
+    erb :message
 end
